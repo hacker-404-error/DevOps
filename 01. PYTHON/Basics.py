f40f1100a875a5e4b0a5bb67c8c73545d43cb0c9
PRITAM DAS <er.pritamdas22@gmail.com>
2024-05-12 19:49:30 +0530

    Updated "python-Variable" Section

--- 01. PYTHON/Basics.py
+++ 01. PYTHON/Basics.py
@@ -1,6 +1,4 @@
-
 #-------------------------------------------- DATA TYPESS -------------------------------------------- 
-
 '''
 Text Type:	str
 Numeric Types:	int, float, complex
@@ -58,10 +56,18 @@ def datatypes():
     m = None
     print(m," : ",type(m),"\n")
 
+#-------------------------------------------- Python Variables -------------------------------------------- 
+'''
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+'''
 def Variables():
     # Get the type
     x = 5
-    print(x,"\n")
+    print(type(x))
 
     # Type Casting
     y = int(x)
@@ -69,17 +75,53 @@ def Variables():
     l = str(x)
     print(y," ",z," ",l," ")
 
+    # Single Quotes Vs Double Quotes
+    x = 'Pritam'
+    y = "Pritam"
+    print(x," Is Equal To: ",y)
+
+    # Case Sensitive
+    x = 4;
+    X = "Apple"
+    print("x = ",x," Does Not Overwrite y = ",X)
+
+    # Ways of Writing Variable
+    # 1. Camel Case: Each word, except the first, starts with a capital letter
+    myFirstVariableName = "Pritam"
+    print(myFirstVariableName,"\n")
+    # 2. Pascal Case: Each word starts with a capital letter.
+    MySecondVariableName = "Das"
+    print(MySecondVariableName,"\n")
+    # 3. Snake Case: Each word is separated by an underscore character
+    My_Third_Variable_Name = "Hello World"
+    print(My_Third_Variable_Name,"\n")
+
+    # Assign Multiple Values
+    x = y = z = "pritam"
+    print(x)
+    print(y)
+    print(z)
+    x,y,z = "I", "AM", "OK"
+    print(x)
+    print(y)
+    print(z)
+    fruits = ["Apple", "Orange", "Banana"]
+    x,y,z = fruits
+    print(x)
+    print(y)
+    print(z)
+
+
 def Strings_Operations():
     a = "Hello World"
     print(a)
 
 
-datatypes()
-Strings_Operations()
+# datatypes()
+Variables()
+# Strings_Operations()
 
-def Strings():
-    #Printing a string
-    print("Hii")
+
 
 
 
